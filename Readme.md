f701e95d92f163a12ba5e3ffc8462f163865129f
Yannick <yannick.spreen@gmail.com>
2023-08-16 09:32:56 -0400

    Init.

---
+++ Readme.md
@@ -0,0 +1,3 @@
+![image](./demo.gif)
+
+Variant A uses safe area insets. Variant B is a VStack. As far as I can tell both have the same behavior.
--- interactive-dismiss-bug/ContentView.swift
+++ interactive-dismiss-bug/ContentView.swift
@@ -7,20 +7,81 @@
 
 import SwiftUI
 
+extension Int: Identifiable { public var id: Int { self } }
+
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+	enum Variant: String, CaseIterable {
+		case A, B
+	}
+
+	@State var variant = Variant.allCases[0]
+
+	var body: some View {
+		content
+			.overlay(alignment: .top) {
+				Picker("Variant", selection: $variant) {
+					ForEach(Variant.allCases, id: \.self) {
+						Text("Variant \($0.rawValue)")
+					}
+				}
+				.pickerStyle(SegmentedPickerStyle())
+				.padding()
+				.background(.white)
+			}
+			.preferredColorScheme(.light)
+	}
+
+	@ViewBuilder
+	var content: some View {
+		switch variant {
+		case .A:
+			VariantA()
+		case .B:
+			VariantB()
+		}
+	}
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+struct VariantA: View {
+	@State var text = ""
+
+	var body: some View {
+		VStack(spacing: 0) {
+			ScrollView {
+				LazyVStack {
+					ForEach(Array(1...100)) {
+						Text("\($0)")
+					}
+				}
+			}
+			.scrollDismissesKeyboard(.interactively)
+			TextField("Input", text: $text)
+				.padding()
+				.background {
+					Color.init(white: 0.9).ignoresSafeArea()
+				}
+		}
+	}
+}
+
+struct VariantB: View {
+	@State var text = ""
+
+	var body: some View {
+		ScrollView {
+			LazyVStack {
+				ForEach(Array(1...100)) {
+					Text("\($0)")
+				}
+			}
+		}
+		.scrollDismissesKeyboard(.interactively)
+		.safeAreaInset(edge: .bottom) {
+			TextField("Input", text: $text)
+				.padding()
+				.background {
+					Color.init(white: 0.9).ignoresSafeArea()
+				}
+		}
+	}
 }
