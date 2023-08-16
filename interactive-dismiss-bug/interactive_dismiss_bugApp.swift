13ce1741f56b4ac5cdcd19305c0869ccea9c511f
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2023-08-16 09:03:36 -0400

    Initial Commit

---
+++ interactive-dismiss-bug/interactive_dismiss_bugApp.swift
@@ -0,0 +1,17 @@
+//
+//  interactive_dismiss_bugApp.swift
+//  interactive-dismiss-bug
+//
+//  Created by Nick on 8/16/23.
+//
+
+import SwiftUI
+
+@main
+struct interactive_dismiss_bugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
